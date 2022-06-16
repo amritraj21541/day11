2f6772c2b7e0b30e0ab0a627fd4e3c3b61302964
amrit <amrit@MSI>
2022-06-16 15:00:11 +0530

    day 11 done

---
+++ day11/src/day11/Demo1.java
@@ -0,0 +1,16 @@
+package day11;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		String str = "Programming";
+
+		String s = new String("World");
+
+		System.out.println(str);
+
+		System.out.println(s);
+
+	}
+
+}
