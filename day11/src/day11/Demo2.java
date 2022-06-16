cb610ff4257891b554c83d643de4adf2eaa8d88d
amrit <amrit@MSI>
2022-06-16 15:12:53 +0530

    day 11 done

---
+++ day11/src/day11/Demo2.java
@@ -0,0 +1,18 @@
+package day11;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		String str = "Hello";
+
+		String str1 = "Hello";
+
+		String str2 = new String("Hello");
+
+		System.out.println(str == str1);
+
+		System.out.println(str == str2);
+
+	}
+
+}
